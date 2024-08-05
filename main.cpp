2323bd004b7c8b29d1c836d652fdd4cbdc949e23
castrogarciajs <johancs.mm@gmail.com>
2024-08-04 21:23:44 -0500

    Add a example with c++

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  cout << "Hola, mundo" << endl;
+
+  int number = 0;
+  string my_string = "Holaaa";
+
+  cout << number << endl;
+  cout << my_string << endl;
+
+  for (int i = 0; i < 10; ++i) {
+    cout << i << endl;
+  }
+
+  return 0;
+}
